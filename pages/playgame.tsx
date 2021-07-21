14f3215cd4be2de3e8884a001ba83907b11467e8
alonso <alonsovillegas2016@gmail.com>
2021-07-21 18:48:47 -0500

    Add head title to playgame page

--- pages/playgame.tsx
+++ pages/playgame.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import { FC } from 'react';
 import Game from '../src/gameRoom/Game';
 
 const playgame:FC = () => {
   return (
-    <Game/>
+    <>
+      <Head>
+        <title>Game Room</title>
+      </Head>
+      <Game/>
+    </>
   );
 }
 
--- src/gameRoom/Game.tsx
+++ src/gameRoom/Game.tsx
@@ -20,7 +20,7 @@ const GameContainer = styled.div`
 export const GameContext = createContext<any>(null);
 
 const Game:FC = () => {
-  const gameMode = 'not-alone';
+  const gameMode = 'alone';
   const players = [1, 2];
   const defoultFirstPlayer = 1;
 
